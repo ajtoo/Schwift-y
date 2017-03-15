fb5ae8ad4214c2a852116281ddabcb8ada8c329b
ajtoo <ajtoo.dev@gmail.com>
2017-03-15 15:35:48 -0700

    Implement basic navbar and reserve space for log in/sign up button

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import MainNav from './shared/main_nav';
 
 const App = ({ children }) => (
   <div>
+    <MainNav></MainNav>
     <h1>SCHWIFTYYY!!!</h1>
+    <button className="btn btn-primary">Is Bootstrap?</button>
     { children }
   </div>
 );
