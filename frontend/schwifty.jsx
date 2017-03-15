c8c13a0bbb362bd221160e00fdf67d43491831ff
ajtoo <ajtoo.dev@gmail.com>
2017-03-14 19:30:12 -0700

    Set up basic routes and Root/App components

--- frontend/schwifty.jsx
+++ frontend/schwifty.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
 //TODO: delete testing imports
 import {signup, login, logout} from './util/session_api_util';
-import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-
+    const store = configureStore();
     //TODO: take testing items off window
     window.signup = signup;
     window.login = login;
     window.logout = logout;
-    window.store = configureStore();
+    window.store = store;
 
-    ReactDOM.render(<h1>Everybody get schwiftyyy!!</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
