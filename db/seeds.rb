43c76f0a52d103c984fc519b6e12c92cb6de59ba
ajtoo <ajtoo.dev@gmail.com>
2017-03-20 00:43:06 -0700

    Add item to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 demo_user = User.create(email: "demo@gmail.com", password: "1234567890")
+Car.create(
+  make: "Dodge",
+  model: "Challenger R/T",
+  year: 2014,
+  mileage: 12898,
+  price: 25500,
+  img_url: "http://res.cloudinary.com/ajtoo/image/upload/v1489995479/Challenger_kqd7zj.jpg"
+)
