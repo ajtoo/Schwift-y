13da863b542745d7ebb013c542e144f06f548f2e
ajtoo <ajtoo.dev@gmail.com>
2017-03-22 12:14:12 -0700

    Start implementation of favorites

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,7 +1,10 @@
 class Api::FavoritesController < ApplicationController
   def create
+    @bench = Bench.create(user_id: params[:user_id], car_id: params[:car_id])
+    
   end
 
   def destroy
+    @bench = Bench.destroy!(user_id: params[:user_id], car_id: params[:car_id])
   end
 end
